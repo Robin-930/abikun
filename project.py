aed269eb1896318a66ed58eabc4abbfc2f73c5cb
Robin-930 <76043672+Robin-930@users.noreply.github.com>
2024-08-22 09:13:27 +0530

    Update project.py

--- project.py
+++ project.py
@@ -1,27 +1,29 @@
 import streamlit as st
 owo = 'kkk'
-st.title("Robin fr life")
-
-st.header("Ahoy!")
-hide_st_style =""""""
+st.title(":blue[Hackathon 2024]  😂", )
+st.write()
+hide_st_style ="""
+<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+header {visibility: hidden;}
+</style>"""
 
 st.markdown(hide_st_style, unsafe_allow_html=True)
 
 
+owo = st.text_input('')
 
-if st.button('Say hello', type= 'secondary'):
-    robin = input("gimme ur money")
-    st.text_input(robin)
-    
-    st.write("why hello there")
-
+if st.button('say hello'):
+    st.write('Hello there' , owo)
+        
 
 with open('gowtham.jpeg', 'rb') as file:
     st.download_button(file_name= 'gowtham.jpeg',
-                       label= 'Gowtham hot pics',
+                       label= 'Gowtham pics',
                        data= file)
 with open('anush.jpeg', 'rb') as file:
     st.download_button(file_name= 'anush.jpeg',
-                       label= 'anush hot pics',
+                       label= 'anush pics',
                        data= file)
     
